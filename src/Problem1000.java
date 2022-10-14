8d14cc37437f1c52405cd4f9188eb52bada78794
Fernando <lfernnado.adriano@gmail.com>
2022-10-14 16:32:10 -0300

    Initial commit

---
+++ src/Problem1000.java
@@ -0,0 +1,15 @@
+public class Problem1000 {
+    /*
+    Welcome to beecrowd!
+    Your first program in any programming language is usually "Hello World!". In this first problem all you have to do is print this message on the screen.
+
+    Input
+    This problem has no input.
+
+     Output
+    You must print the message Hello World! and then the endline as shown below.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
