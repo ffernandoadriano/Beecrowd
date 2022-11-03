227f40bea67f99fc584f7f34fc4f40dc8d95ba61
Fernando <lfernnado.adriano@gmail.com>
2022-11-03 12:46:41 -0300

    add Problem1019.

---
+++ src/Problem1019.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+    /*
+    Read an integer value, which is the duration in seconds of a certain event in a factory, and inform it expressed in hours:minutes:seconds.
+
+    Input
+    The input file contains an integer N.
+
+    Output
+    Print the read time in the input file (seconds) converted in hours:minutes:seconds like the following example.
+     */
+public class Problem1019 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int durationSeconds, second, minute, hours;
+
+        durationSeconds = sc.nextInt();
+
+        second = durationSeconds % 60;
+        minute = ((durationSeconds - second) / 60) % 60;
+        hours = (durationSeconds - minute) / 3600;
+
+        System.out.printf("%d:%d:%d%n", hours, minute, second);
+
+        sc.close();
+    }
+}
