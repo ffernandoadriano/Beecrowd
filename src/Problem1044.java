1b0ef7900bda10babbb9631bf613e4a0f4f19a3d
Fernando Adriano <lfernando.adriano@gmail.com>
2023-01-17 22:14:38 -0300

    add Problem1044.

---
+++ src/Problem1044.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+    /*
+    Read two integer values (A and B). After, the program should print the message "Sao Multiplos" (are multiples) or "Nao sao Multiplos" (aren’t multiples), corresponding to the read values.
+
+    Input
+    The input has two integer numbers.
+
+    Output
+    Print the relative message to the input as stated above.
+     */
+public class Problem1044 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        if (a % b == 0 || b % a == 0) {
+            System.out.println("Sao Multiplos");
+        } else {
+            System.out.println("Nao sao Multiplos");
+        }
+
+        sc.close();
+    }
+}
